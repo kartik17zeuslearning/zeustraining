28b783e4373b2e4e653ca360025318d845bff169
Kartik Rokade <kartik.rokade@zeuslearning.mumbai>
2022-07-12 23:21:24 +0530

    added dropdown submenu to hamburger menu

---
+++ Task4_login_page/navbar.js
@@ -0,0 +1,16 @@
+function contentFunction() {
+    document.getElementById("contentid").classList.toggle("show");
+  }
+
+function userFunction() {
+    document.getElementById("userid").classList.toggle("show");
+  }
+
+function reportFunction() {
+    document.getElementById("reportid").classList.toggle("show");
+  }
+
+function adminFunction() {
+    document.getElementById("adminid").classList.toggle("show");
+  }
+  
